7ce88c01526c3a451dbb6b876bcbc4fff8828a16
Bruno Amezcua <brunoamezcua@Brunos-MacBook-Pro.local>
2019-11-24 08:49:04 -0600

    Initial commit

---
+++ P&BStorage/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>P&BStorage</title>
+    <link rel="stylesheet" href="css/style.css">
+    <?php require 'php/main.php';?>
+    <script src='js/main.js'></script>
+  </head>
+</html>
